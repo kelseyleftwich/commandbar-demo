e43f26c5bc7f61304774f5a13ca602e2c4f65936
Kelsey Leftwich <kelsey.leftwich@gmail.com>
2023-10-16 14:46:14 -0500

    adds extra html to artist commandbar record

--- providers/CommandBar.tsx
+++ providers/CommandBar.tsx
@@ -18,14 +18,53 @@ type Response = {
   };
 };
 
+type ArtistResponse = {
+  data: {
+    id: number;
+    title: string;
+    birth_date: number;
+  }[];
+  config: {
+    iiif_url: string;
+  };
+};
+
+const getExtraHtml = (domain: string, channel: string) =>
+  `<div style="display: grid; grid-template-columns: auto auto; gap: 1rem; font-size: 13px; align-items: center; padding-bottom: 0.5rem;">
+  <div>${domain}</div>
+  <div style="
+  border-radius: 4px;
+  padding-left: 8px;
+  padding-right: 8px;
+  font-size: 10px;
+  border: 1px solid rgb(189, 189, 189);
+  ">${channel}</div>
+  </div>
+  `;
+
 const onSearchArtists = async (query: string) => {
-  const res = await fetch(
-    `https://api.artic.edu/api/v1/artists/search?query[term][is_public_domain]=true&query[match][title]=${query}&fields=title,id`
-  );
+  let elasticSearchQuery: any = {
+    query: {
+      match: {
+        title: query,
+      },
+    },
+    limit: 12,
+    fields: ["title", "id", "birth_date"],
+  };
 
-  const resJson: Response = await res.json();
+  const baseUrl = "https://api.artic.edu/api/v1/artists/search?";
+  const formattedQuery = encodeURIComponent(JSON.stringify(elasticSearchQuery));
+  const urlToFetch = `${baseUrl}params=${formattedQuery}`;
 
-  return resJson.data;
+  const res = await fetch(urlToFetch);
+
+  const resJson: ArtistResponse = await res.json();
+
+  return resJson.data.map((artist) => ({
+    ...artist,
+    __extraHTML: getExtraHtml(artist.title, artist.birth_date.toString()),
+  }));
 };
 
 // API does not have search endpoint for artwork types so we query all and then sort
